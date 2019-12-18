7201b003fae512a2bdb6475023f2be934a7d1ae5
DrEMartstud <dremartstud@gmail.com>
2019-12-18 19:49:15 +0200

    Initial Commit

---
+++ RecipesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecipesApp
+//
+//  Created by Андрей Головченко on 18.12.2019.
+//  Copyright © 2019 DrEMartstud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
